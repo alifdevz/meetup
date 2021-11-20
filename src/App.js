91382b62740aaaa40dada2bd1200ac5940b446d7
alifdevz <alifm.devz@gmail.com>
2021-11-20 12:38:39 +0800

    Added routing + Added Main Navigation.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+import AllMeetUpsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import Favorites from "./pages/Favorites";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<AllMeetUpsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
