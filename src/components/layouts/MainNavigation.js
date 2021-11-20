2ea63c8a89b04a7bbf61c0119fd7d011296d6963
alifdevz <alifm.devz@gmail.com>
2021-11-20 13:10:39 +0800

    Added styling to the navigation bar

--- src/components/layouts/MainNavigation.js
+++ src/components/layouts/MainNavigation.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 
+import classes from "./MainNavigation.module.css";
+
 function MainNavigation() {
   return (
-    <header>
-      <div>React Meetupps</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetupps</div>
       <nav>
         <ul>
           <li>
