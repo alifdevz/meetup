cceaa50e8f6fc60ffd0000d0a691c27c5c179d8c
alifdevz <alifm.devz@gmail.com>
2021-11-20 22:25:04 +0800

    Use props to populate MeetupItem data dynamically + Added MeetupItem.module.css and MeetupList.module.css

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,17 +1,19 @@
 function MeetupItem(props) {
-    return <li>
-        <div>
-            <img src='' alt='' />
-        </div>
-        <div>
-            <h3>TITLE</h3>
-            <address>ADDRESS</address>
-            <p>DESCRIPTION</p>
-        </div>
-        <div>
-            <button>To Favorites</button>
-        </div>
+  return (
+    <li>
+      <div>
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div>
+        <h3>{props.title}</h3>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+      </div>
+      <div>
+        <button>To Favorites</button>
+      </div>
     </li>
+  );
 }
 
 export default MeetupItem;
