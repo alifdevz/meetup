90a37c92fc2cd7ee3fbbdafe1d993880bb0531b2
alifdevz <alifm.devz@gmail.com>
2021-11-20 13:36:38 +0800

    Added data.dummy.js

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,9 @@
+const DUMMY_DATA = require('../data/data.dummy.js');
+
 function AllMeetUpsPage() {
-    return <div>All Meetups Page</div>;
+    return <section>
+        <h1>All Meetups</h1>
+    </section>;
 }
 
 export default AllMeetUpsPage;
