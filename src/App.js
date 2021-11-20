578fc2ec85b96aebd68740de5a23ef522ed2f63a
alifdevz <alifm.devz@gmail.com>
2021-11-20 12:51:30 +0800

    MainNavigation as header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
-import AllMeetUpsPage from "./pages/AllMeetups";
-import NewMeetupPage from "./pages/NewMeetup";
-import Favorites from "./pages/Favorites";
+import AllMeetUpsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import Favorites from './pages/Favorites';
+import MainNavigation from './components/layouts/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetUpsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
