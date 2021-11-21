01b1f5dccde47632d269b14f177b2c5f18521290
alifdevz <alifm.devz@gmail.com>
2021-11-21 11:13:30 +0800

    Wrap Routes component and all pages components into Layout component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import { Route, Routes } from 'react-router-dom';
 import AllMeetUpsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import Favorites from './pages/Favorites';
-import MainNavigation from './components/layouts/MainNavigation';
+import Layout from './components/layouts/Layout';
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetUpsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
         <Route path="/favorites" element={<Favorites />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
